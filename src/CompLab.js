24ead4c28398f65c3a1b93211ff6320e72e3cee5
Olive <njerio86@gmail.com>
2023-05-21 16:31:59 +0300

    added a carousel of photos to CompLab.js

--- src/CompLab.js
+++ src/CompLab.js
@@ -1,9 +1,80 @@
 import React from "react";
+import Comp1 from "./Images/comp1.jpg";
+import Comp2 from "./Images/comp2.jpg";
+import Comp3 from "./Images/comp3.jpg";
 
 function CompLab() {
   return (
     <div>
       <h3>COMPUTER LABORATORY</h3>
+      <div
+        id="carouselExampleAutoplaying"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src={Comp2}
+              class="d-block w-100"
+              alt="Comps photos"
+              height={550}
+              width={550}
+            />
+            <div class="carousel-caption d-none d-md-block">
+              <h5 className="custom-heading">
+                Ngami Secondary School Computer Lab
+              </h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              src={Comp1}
+              class="d-block w-100"
+              alt="Computer lab photos"
+              height={550}
+              width={550}
+            />
+            <div class="carousel-caption d-none d-md-block">
+              <h5 className="custom-heading">
+                Ngami Secondary School Computer Lab
+              </h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              src={Comp3}
+              class="d-block w-100"
+              alt="Comp lab photos"
+              height={550}
+              width={550}
+            />
+            <div class="carousel-caption d-none d-md-block">
+              <h5 className="custom-heading">
+                Ngami Secondary School Computer Lab
+              </h5>
+            </div>
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
       <p>
         Ngami Secondary School prides itself on providing quality education to
         its students. One of the school's essential facilities is the computer
